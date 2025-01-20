b2412bed5266118b9fe092207fa03f2c2ef4771e
健豪 陈 <1663914536@qq.com>
2025-01-20 19:36:11 +0800

    添加 cmake

---
+++ test.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "tools/module/thread.hpp"
+#include "tools/module/big_number.hpp"
+#include "tools/module/data.hpp"
+#include "tools/module/time.hpp"
+
+int main() {
+
+    return 0;
+}
